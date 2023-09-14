a0a00905e598f7d488737cbde3375c2c80b9540c
Firman Ramadhan <firman19ramadhan@gmail.com>
2023-09-14 15:38:36 +0700

    pushhhh

---
+++ src/app/post-a-job/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import React, { FC } from 'react'
+
+interface PostProps {
+
+}
+
+const Post: FC<PostProps> = ({ }) => {
+    return (
+        <div>
+            PostJobPage
+        </div>
+    )
+}
+
+export default Post
